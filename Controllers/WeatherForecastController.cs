c6db9f80b4c9e3436d08358bfae7eab591ec4726
javasorn <javasorn@gmail.com>
2021-05-26 16:48:23 +0700

    MyFirstAzureAPI and DbUp

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyFirstAzureWebApp.Business;
+using MyFirstAzureWebApp.Models;
+using System.Collections.Generic;
+
+namespace MyFirstAzureWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private Weather weather;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+
+            _logger = logger;
+            weather = new Weather();
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return weather.Forecast();
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<WeatherForecast> GetById(string id)
+        {
+            return weather.ForecastById(id);            
+        }
+    }
+}
---
+++ Database.Bootstrap/Program.cs
@@ -0,0 +1,46 @@
+﻿using DbUp;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Database.Bootstrap
+{
+    class Program
+    {
+        // DbUp
+        // https://dbup.readthedocs.io/en/latest/
+
+        static void Main(string[] args)
+        {
+            var connectionString = args.FirstOrDefault()
+                    ?? "Initial Catalog = MyApp; Data Source =.; Integrated Security = True; Pooling = true";
+            
+            // If you want your application to create the database for you, add the following line after the connection string:
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+        }
+    }
+}
