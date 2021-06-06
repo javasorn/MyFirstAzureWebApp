8f850ca347c5a42416244d0844f59d96f4ba3b49
javasorn <javasorn@gmail.com>
2021-06-06 08:33:00 +0700

    JWT Authentication

---
+++ Controllers/FooController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstAzureWebApp.Authentication;
+using MyFirstAzureWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstAzureWebApp.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FooController : ControllerBase
+    {
+        private readonly IJWTAuthenticationManager jWTAuthenticationManager;
+
+        public FooController(IJWTAuthenticationManager jWTAuthenticationManager)
+        {
+            this.jWTAuthenticationManager = jWTAuthenticationManager;
+        }
+
+        // GET: api/Name
+        [Authorize]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Sydney", "London" };
+        }
+
+        // GET: api/Name/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "Sydney";
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCredential userCred)
+        {
+            var token = jWTAuthenticationManager.Authenticate(userCred.Username, userCred.Password);
+
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(token);
+        }
+
+    }
+
+}
--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MyFirstAzureWebApp.Authentication;
+using System.Text;
 
 namespace MyFirstAzureWebApp
 {
@@ -30,8 +27,32 @@ namespace MyFirstAzureWebApp
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
+                c.OperationFilter<CustomHeaderSwaggerAttribute>();
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyFirstAzureWebApp", Version = "v1" });
             });
+
+            var tokenKey = Configuration.GetValue<string>("TokenKey");
+            var key = Encoding.ASCII.GetBytes(tokenKey);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddSingleton<IJWTAuthenticationManager>(new JWTAuthenticationManager(tokenKey));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +68,8 @@ namespace MyFirstAzureWebApp
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
