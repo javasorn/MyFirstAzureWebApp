4a2d12adbc218bbd36e6c98b74719a6552236722
javasorn <javasorn@gmail.com>
2021-06-16 11:22:45 +0700

    add partial

---
+++ Entity/partial/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public partial class Employee
+    {
+        public string PhotoPath2 { get; set; }
+    }
+}
