013dc4b53031b209b62b71866217aee1c9372c5a
javasorn <javasorn@gmail.com>
2021-09-24 10:50:06 +0700

    AzureBlobStorage with nlog

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
 using NLog.Web;
 using System;
 using System.Collections.Generic;
@@ -18,34 +19,53 @@ namespace MyFirstAzureWebApp
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("Created HostBuilder");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureLogging(logging =>
                 {
+                    Console.WriteLine("ConfigureLogging");
                     logging.ClearProviders();
                     logging.AddConsole();
-                    logging.SetMinimumLevel(LogLevel.Trace);
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
                 })
                 .ConfigureAppConfiguration((context, config) =>
                 {
                     var settings = config.Build();
                     if (!context.HostingEnvironment.IsDevelopment())
                     {
+                        Console.WriteLine("Get AzureKeyVaultEndpoint");
                         var keyVaultEndpoint = settings["AzureKeyVaultEndpoint"];
+                        Console.WriteLine("keyVaultEndpoint : " + keyVaultEndpoint);
                         if (!string.IsNullOrEmpty(keyVaultEndpoint))
                         {
                             var azureServiceTokenProvider = new AzureServiceTokenProvider();
                             var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
                             config.AddAzureKeyVault(keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
                         }
+                        //add new
+                        UpdateNLogConfig(config.Build(), context.HostingEnvironment);
                     }
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    Console.WriteLine("Startup");
                     webBuilder.UseStartup<Startup>();
                 })
                 .UseNLog();
+
+        //add new
+        private static void UpdateNLogConfig(IConfigurationRoot configuration, IHostEnvironment env)
+        {
+            Console.WriteLine("UpdateNLogConfig");
+            var storageConnectionString = configuration.GetSection("dev-uat-storage-saleonmob").Get<string>();
+            Console.WriteLine("storageConnectionString:"+ storageConnectionString);
+            GlobalDiagnosticsContext.Set("StorageConnectionString", storageConnectionString);
+            var configFile = env.IsDevelopment() ? "nlog.{env.EnvironmentName}.config" : "nlog.config";
+            Console.WriteLine("configFile:" + configFile);
+            LogManager.Configuration = LogManager.LoadConfiguration(configFile).Configuration;
+        }
     }
 }
